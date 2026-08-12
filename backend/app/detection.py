69cc6dcc075678a3a8e20962f1ff0d751fab1499
20055568 <lohithbadam10@gmail.com>
2026-08-12 23:28:31 +0100

    Add vehicle classes for detection

--- backend/app/detection.py
+++ backend/app/detection.py
@@ -2,3 +2,6 @@ from typing import List, Dict, Tuple
 import cv2
 import numpy as np
 from .config import USE_YOLO
+
+VEHICLE_CLASSES = {'car', 'truck', 'bus', 'motorcycle', 'motorbike'}
+_yolo_model = None
